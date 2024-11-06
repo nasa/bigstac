1713f7d1025e1a09a506f568b6edff99fd580fd2
Thomas Cherry <thomas.a.cherry@nasa.gov>
2024-11-06 15:23:35 -0500

    try this version

--- analyze/merge.py
+++ analyze/merge.py
@@ -10,20 +10,24 @@ import time
 import dask.dataframe as dd
 import dask_geopandas as dgp
 from dask.distributed import LocalCluster
+import pandas as pd
+import numpy as np
 
 cluster = LocalCluster(n_workers=1,
-    threads_per_worker=2,
+    threads_per_worker=1,
     processes=False,
-    dashboard_address=':8787')
-#, memory_limit="2GiB")
+    dashboard_address=':8787',
+    memory_limit="2GiB")
 client = cluster.get_client()
 
 print("start")
 ddf = dgp.read_parquet('polygons_30m.parquet', gather_spatial_partitions=False)
 #ddf.compute()
-ddf.ddf.persist()
+#ddf.persist()
+
+ddf['bbox'] = ddf.geometry.apply(lambda geom: geom.bounds, meta=('bbox', 'object'))
 
 print('write')
-ddf.to_parquet('polygons_30m_bbox.parquet', write_covering_bbox=True, row_group_size=120950)
+ddf.to_parquet('polygons_30m_bbox.parquet', write_index=False, row_group_size=120950)
 
 print("done")
