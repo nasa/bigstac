4e98d983ff780083481c34ca91a85614057ec712
Johnathan Rush <10775395+rushgeo@users.noreply.github.com>
2025-02-14 09:57:00 -0500

    CMR-10352: initial version of log parser

---
+++ cmr_logs_categorizer/log_to_table.R
@@ -0,0 +1,272 @@
+# Given an uncompressed export file of CMR logs, extract and format elements
+# that are useful for understanding categories of CMR queries.
+# Stores output in tabular format.
+
+library(jsonlite)
+library(data.table)
+library(stringr)
+library(sf)
+library(arrow)
+
+source('parsing_functions.R')
+
+# Capture command line arguments
+args <- commandArgs(trailingOnly = TRUE)
+
+# XXX DEBUG
+if(length(args) == 0){
+  args = c('prod-app-047c4bf7c23d46af8346597855880020/000000.json')
+}
+
+# Check if arguments were provided
+if (length(args) != 1) {
+  stop("Must provide path to log data as an argument", call. = FALSE)
+}
+
+# Get full path to prepared log file
+in_full_path = args[1]
+if(!file.exists(in_full_path)) stop(paste(
+  "File", in_full_path, "does not exist."), call. = FALSE)
+
+# Parse path and filename; use these later for writing output
+in_path = dirname(in_full_path)
+in_file = basename(in_full_path)
+
+# Read input file and convert to data.table
+dt = as.data.table(fromJSON(in_full_path), flatten = TRUE)
+# Add row IDs
+dt[, id := .I]
+
+# Columns to retain that don't need transformed ----
+# Note "now" is a string and would need converted to time for analysis
+columns_keep = c("now", "status", "method", "client.id")
+
+# ///////////////////////////////////////
+# Process root properties            ----
+# ///////////////////////////////////////
+
+## Get requested concepts and formats from the URI ----
+
+dt[, concept := getPathByPos(uri, 1)]
+# Group all PHP requests together
+dt[str_detect(concept, 'php'), concept := 'PHP'] 
+# Remove extensions (formats) from concepts
+dt[str_detect(concept, '\\.'), concept := str_split_i(concept, '\\.', 1)]
+dt[ , format := matchExtension(uri)]
+# XXX CONSIDER FOR REPORT
+dt[, .N, by = concept][order(-N)]
+dt[, .N, by = format][order(-N)]
+
+## Extract and simplify user agent ----
+dt[, user_agent_type := str_split_i(substr(user.agent,1,80),'/',1)]
+dt[, user_agent_type := str_split_i(user_agent_type, "\\(", 1)]
+dt[str_detect(user_agent_type, "podaac-subscriber"), 
+   user_agent_type := "podaac-subscriber"]
+dt[, user_agent_type := str_split_i(user_agent_type, " v[[:digit:]]", 1)]
+dt[str_detect(user_agent_type, "[B|b]ot"), user_agent_type := "Bot"]
+dt[str_detect(user_agent_type, "[P|p]ython|PycURL"), user_agent_type := "Python"]
+# XXX CONSIDER FOR REPORT
+dt[, .N, by = user_agent_type][order(-N)]
+
+## Convert cmr.took to numeric type ----
+dt[, cmr_took := as.numeric(cmr.took)]
+# Remove the old version with . in the name to avoid confusion
+dt[, cmr.took := NULL]
+# XXX CONSIDER FOR REPORT
+summary(dt$cmr_took)
+
+## Record whether cmr.search.after was provided ----
+dt[, used_search_after := !is.na(cmr.search.after)]
+# XXX CONSIDER FOR REPORT
+dt[, .N, by = used_search_after]
+
+# ///////////////////////////////////////
+# Process temporal queries           ----
+# ///////////////////////////////////////
+
+## Combine temporal queries to a single column ----
+
+# Columns with trailing '..' can be array versions of parameters, although
+# sometimes they only contain single values. Because they do not always store
+# multiple values, we'll check the column type before processing them
+columns_temporal = c("query.params.temporal", "query.params.temporal..", 
+                     "form.params.temporal", "form.params.temporal..")
+# Column names are hard-coded, but in rare cases may not be present in logs
+columns_temporal = intersect(columns_temporal, names(dt)) 
+
+for(column_name in columns_temporal){
+  column_type = class(dt[, get(column_name)])
+
+  if(column_type == "list"){
+    # for rows with at least one temporal query...
+    dt[sapply(get(column_name), length) > 0, 
+       # copy them to the new "time_query" column...
+       time_query := sapply(  
+         get(column_name), 
+         # combining multiple temporal queries into a single ;-separated string
+         function(x){
+           paste(unlist(x), collapse = ";")
+         })] 
+  } else {
+    # for rows with a temporal query...
+    dt[!is.na(get(column_name)), 
+       # copy them to the new "time_query" column
+       time_query := get(column_name)] 
+  }
+}
+
+## Add temporal facets to time column ----
+columns_temporal_facets = grep("params.temporal_facet.*(year|month|day)",
+                               names(dt), ignore.case = TRUE, value = TRUE)
+
+# Combine the facet columns into a single date column
+melt_time = melt(dt, id.vars='id', 
+                 measure.vars = columns_temporal_facets)[,
+                   .(facet_date = paste(value, collapse = '-')), by = id]
+# Convert literal NA strings to NA value
+melt_time[facet_date == "NA-NA-NA", facet_date := NA_character_]
+# Remove NA portions of the facet date, keeping the year or year and month
+melt_time[str_detect(facet_date, "NA"), 
+          facet_date := str_split_i(facet_date, "-NA", 1)]
+# Insert dates from facets into the temporal query column where it doesn't already have values
+dt[is.na(time_query), time_query := melt_time[.SD, facet_date, on = .(id)]]
+
+# XXX CONSIDER FOR REPORT
+dt[, .N, by = .(has_temporal_query = !is.na(time_query))]
+
+# ///////////////////////////////////////
+# Process form-params & query-params ----
+# ///////////////////////////////////////
+
+## Provider ----
+
+columns_provider = grep('params.provider', ignore.case = TRUE, names(dt), 
+                        value = TRUE)
+dt[, provider := combine_columns_get_nonNA(.SD, columns_provider, TRUE)]
+
+# XXX CONSIDER FOR REPORT
+dt[, .N, by = provider][order(-N)]
+
+## Sort key ----
+
+columns_sortkey = grep('params.sort_key', ignore.case = TRUE, names(dt),
+                       value = TRUE)
+dt[, sort_key := combine_columns_get_nonNA(.SD, columns_sortkey, TRUE)]
+
+# XXX CONSIDER FOR REPORT
+dt[, .N, by = sort_key][order(-N)]
+
+## Page size page num ----
+columns_page_size = grep('params.(page_size|pageSize)', ignore.case = TRUE, 
+                         names(dt), value = TRUE)
+dt[, page_size := as.integer(
+  combine_columns_get_nonNA(.SD, columns_page_size, TRUE))]
+
+columns_page_num = grep('params.page_num', ignore.case = TRUE, names(dt), 
+                         value = TRUE)
+dt[, page_num := as.integer(
+  combine_columns_get_nonNA(.SD, columns_page_num, TRUE))]
+
+# XXX CONSIDER FOR REPORT
+dt[, .N, by = page_size][order(-N)]
+dt[, .N, by = page_num][order(-N)]
+
+## Version ----
+columns_version = grep('params.version', ignore.case = TRUE, names(dt), 
+                         value = TRUE) # exludes options.version..pattern
+dt[, version := combine_columns_get_nonNA(.SD, columns_version, TRUE)]
+
+# XXX CONSIDER FOR REPORT
+dt[, .N, by = version][order(-N)]
+
+## Short name ----
+columns_short_name = grep('params.short_name', ignore.case = TRUE, names(dt), 
+                        value = TRUE)
+dt[, short_name := combine_columns_get_nonNA(.SD, columns_short_name, TRUE)]
+
+# XXX CONSIDER FOR REPORT
+dt[, .N, by = short_name][order(-N)]
+
+## Concept ID ----
+columns_concept_id = grep('params.concept_id|params.collection_concept_id', 
+                          ignore.case = TRUE, names(dt), value = TRUE)
+dt[, concept_id := combine_columns_get_nonNA(.SD, columns_short_name, TRUE)]
+
+# XXX CONSIDER FOR REPORT
+dt[, .N, by = concept_id][order(-N)]
+
+## Instrument ----
+columns_instrument = grep('params.instrument', ignore.case = TRUE, names(dt), 
+                          value = TRUE)
+dt[, instrument := combine_columns_get_nonNA(.SD, columns_instrument, TRUE)]
+
+# XXX CONSIDER FOR REPORT
+dt[, .N, by = instrument][order(-N)]
+
+# ///////////////////////////////////////
+# Process spatial queries            ----
+# ///////////////////////////////////////
+# Note the order that we're processing spatial types below is intentional. Each
+# type will only update the `wkt` values that have not yet been set (are NA). We
+# start with more complex shapes and end with simpler shapes. This means that,
+# for example, if a user submitted both a polygon and a point, we keep only the
+# polygon.
+
+# Initialize `wkt` column with NA values
+dt[, wkt := NA_character_]
+
+## Polygons ----
+columns_polygon = grep('params.polygon', ignore.case = TRUE, names(dt), 
+                       value = TRUE)
+for(this_column in columns_polygon){
+  dt[is.na(wkt), wkt := cmr_to_wkt(get(this_column), geo_type = "POLYGON")]
+}
+
+## Bounding boxes ----
+columns_bbox = grep('bounding.{0,1}box', ignore.case = TRUE, names(dt), value = TRUE)
+for(this_column in columns_bbox){
+  convert_bbox_column(dt, this_column, "wkt")
+}
+
+## Circles ----
+columns_circles = grep('circle', ignore.case = TRUE, names(dt), value = TRUE)
+for(this_column in columns_circles){
+  calculate_circle_columns(dt, this_column, "wkt", "circle_radius")
+}
+
+## Lines ----
+columns_lines = grep('params.line', ignore.case = TRUE, names(dt), value = TRUE)
+for(this_column in columns_lines){
+  dt[is.na(wkt), wkt := cmr_to_wkt(get(this_column), geo_type = "LINESTRING")]
+}
+
+## Points ----
+columns_points = grep('params.point', ignore.case = TRUE, names(dt), 
+                     value = TRUE)
+for(this_column in columns_points){
+  dt[is.na(wkt), wkt := cmr_to_wkt(get(this_column), geo_type = "POINT")]
+}
+
+# Address spatial errors ----
+# `string_to_wkt` writes errors to output, which WKT readers can't handle
+# Move those errors to another column
+dt[str_detect(wkt, "ERROR"), error := wkt]
+dt[str_detect(wkt, "ERROR"), wkt := NA_character_]
+
+# ///////////////////////////////////////
+# Write to file                      ----
+# ///////////////////////////////////////
+# TODO: append to `columns_transformed` throughout the script instead of all at once
+columns_transformed = c("id", "concept", "format", "user_agent_type", "cmr_took",
+                        "used_search_after", "time_query", "provider", "sort_key",
+                        "page_size", "page_num", "version", "short_name", 
+                        "concept_id", "instrument", "wkt", "circle_radius")
+final_columns = c(columns_keep, columns_transformed, "error")
+final_columns = intersect(final_columns, names(dt))
+
+dtSub = dt[, ..final_columns]
+
+out_file_name = paste0(str_split_i(in_file, pattern = "\\.", 1),
+                  ".parquet")
+out_file_full_path = paste(in_path, out_file_name, sep = "/")
+write_parquet(dtSub, out_file_full_path)
---
+++ cmr_logs_categorizer/parsing_functions.R
@@ -0,0 +1,318 @@
+# Functions to support log_to_table.R
+
+extensions = c('html', 'json', 'xml', 'echo10', 'iso', 'iso19115', 'dif',
+               'dif10', 'csv', 'atom', 'opendata', 'kml', 'stac', 'native', 
+               'umm-json', 'umm_json')
+extensionExpression = paste0('.', extensions, collapse = '|')
+
+#' Matches the specified data format from the input URI
+#'
+#' @param uri URI string
+#'
+#' @returns string contianing the matched data format
+matchExtension <- function(uri){
+  lastPart = sapply(uri, function(uri){
+    tempCount = str_count(uri, '/')
+    if (is.na(tempCount)) { print(paste("NA for:", uri))}
+    str_split_i(uri, "/", tempCount + 1)})
+  matched = str_match(lastPart, extensionExpression)
+  return(str_split_i(matched, '\\.', 2)) # remove period
+}
+
+
+#' Get the specified portion of an input URI
+#'
+#' @param uri URI string
+#' @param position numeric, the position of the desired portion of the URI
+#'
+#' @returns string of the specified position of the path
+getPathByPos <- function(uri, position = 1){
+  split <- sapply(uri, function(x) {
+    if (str_detect(x, "/search/")){
+      return(str_split_i(x, "/search/", 2))
+    } else {
+      return(x)
+    }
+  })
+  str_split_i(split, "/", position)
+}
+
+
+#' Combine columns into a single string
+#' 
+#' @param dt data.table with the columns to combine
+#' @param columns_char a character vector containing names of columns that should be combined
+#' @param first If TRUE, return the first non-NA value. If FALSE, paste all column values into a single string (default: FALSE)
+#' 
+#' @return A character vector representing a column of strings combined from the input columns
+combine_columns_get_nonNA <- function(dt, columns_char, first = FALSE){
+  dtSub = dt[, ..columns_char]
+  # Handle list type columns, combining their values into single strings per row
+  final_columns = columns_char
+  for (this_column in columns_char){
+    if (class(dtSub[, get(this_column)]) == "list"){
+      new_column = paste0(this_column, "_unlist")
+      dtSub[, (new_column) := sapply(get(this_column), function(x){
+        csv = paste(unlist(x), collapse = ',') 
+        csv[sapply(csv, nchar) == 0] <- NA_character_
+        csv
+      })]
+      final_columns = final_columns[final_columns != this_column]
+      final_columns = c(final_columns, new_column)
+    }
+  }
+  dtSub = dtSub[, ..final_columns]
+  
+  # Combine the columns into a single string per row
+  if (first){
+    strings = apply(dtSub, 1, function(x) first(as.character(na.omit(x))))
+  } else {
+    strings = apply(dtSub, 1, function(x) paste(na.omit(x), collapse = ';'))
+  }
+  
+  # Replace zero length character vectors with NA values
+  strings[sapply(strings, length) == 0] <- NA_character_
+  
+  # Change column from list type to character type
+  if(class(strings) == 'list'){
+    old_len = length(strings)
+    strings <- unlist(strings, recursive = FALSE)
+    new_len = length(strings)
+    if(new_len != old_len) stop(
+      paste("Lengths for combination of", paste(columns_char, collapse = " & "), 
+            "don't match after unlisting"))
+  }
+  strings
+}
+
+
+#' Given a column (list or character type) of coordinates and a geospatial type,
+#' return character vector of WKT
+#'
+#' @param column input column (list or character vector) containing coordinates
+#' @param geo_type one of "POLYGON", "LINESTRING", or "POINT"
+#'
+#' @returns character vector of well-known text (WKT) versions of input geometry
+cmr_to_wkt <- function(column, geo_type){
+  # POLYGON, POINT, LINE
+  if(class(column) == "character"){
+    # Convert every feature to WKT
+    coords = sapply(column, string_to_wkt, geo_type = geo_type,
+                    USE.NAMES = FALSE)
+    # Complete WKT by prefixing the geometry type
+    coords = sapply(coords, function(x){
+      if(is.null(x)) NA_character_ else paste(geo_type, x)
+    })
+  # MULTIPOLYGON, MULTIPOINT
+  } else if (class(column) == "list"){
+    # For every feature...
+    coord_list = lapply(column, function(coord_array){
+      # Convert each ring using `string_to_wkt` individually
+      sapply(coord_array, string_to_wkt, geo_type = geo_type,
+             USE.NAMES = FALSE)
+    })
+    # Combine all rings into one comma separated list prefixed by geometry type
+    coords = sapply(coord_list, 
+                    function(single_feature){
+                      if(length(single_feature) == 0){
+                        return(NA_character_)
+                      } else {
+                        paste0("MULTI", geo_type, " (", 
+                               paste(unlist(single_feature), collapse = ", "), 
+                               ")")
+                      }
+                    })
+  } else {
+    stop(paste("ERROR: column", column, "is", class(column), 
+               "type (not chracter or list)"))
+  }
+  coords
+}
+
+
+#' Given a single string of coordinates and a geospatial type, return WKT
+#'
+#' @param text CSV string of coordinates in pattern X,Y,X,Y,...
+#' @param geo_type feature type in all capital letters
+#'
+#' @returns a partially-formatted WKT string of the feature (without the geo_type prefix)
+string_to_wkt <- function(text, geo_type){
+  # Early exit if given NA data
+  if(is.na(text)) return(NULL)
+  
+  # Split all commas
+  all_split = str_split_1(text, ',')
+  # Make sure there's an even number of elements
+  if(length(all_split)%%2 == 1){
+    return("ERROR: Odd Coordinate Count")
+  } else if("" %in% all_split){
+    return("ERROR: Blank coordinate")
+  }
+  
+  # Retrieve two coordiantes at a time into a new string
+  coordinate_pairs = sapply(1:(length(all_split)/2), function(i){
+    toID = i*2
+    this_pair <- paste(all_split[(toID-1):toID], collapse = " ")
+    this_pair
+  })
+  
+  # Gather coordiante pairs into WKT format
+  if(geo_type == "POLYGON"){
+    paste0("((", paste(coordinate_pairs, collapse = ", "), "))")
+  } else {
+    paste0("(", paste(coordinate_pairs, collapse = ", "), ")")
+  }
+}
+
+
+#' Convert bounding box to polygon as well-known text (WKT)
+#'
+#' @param column input column (list or character vector) containing bounding box
+#'   data
+#'
+#' @returns column (character vector) of WKT values
+bbox_to_polygon <- function(column){
+  # Turn off S2 geometry library to avoid conversion of worldwide bounding boxes
+  # to "POLYGON FULL"
+  initial_s2_state = sf_use_s2()
+  if(initial_s2_state) suppressMessages(sf_use_s2(FALSE))
+  # Run anonymous function on every logged bounding box query
+  bbox_features = lapply(column, function(value){
+    # Inner lapply enables handling lists of multiple bounding boxes per query
+    lapply(value, function(v){
+      # Convert CSV string to four numbers
+      bbox_num = as.numeric(str_split_1(v, pattern = ','))
+      # Convert numeric vector to bounding box object
+      bbox = st_bbox(c(
+        xmin = bbox_num[1], 
+        xmax = bbox_num[3], 
+        ymax = bbox_num[4], 
+        ymin = bbox_num[2]), crs = st_crs(4326)
+      )
+      # Return polygon version of bounding box
+      st_as_sfc(bbox)
+    }
+    )
+  })
+  out_wkt = sapply(bbox_features, function(x){
+    # Convert (multi)polygon versions of bounding boxes to WKT
+    geom_list = st_sfc(unlist(x, recursive = FALSE))
+    if(length(geom_list) > 1){
+      st_as_text(st_combine(geom_list)) # MULTIPOLYGON
+    } else {
+      st_as_text(geom_list) # POLYGON
+    }
+  })
+  if(initial_s2_state) suppressMessages(sf_use_s2(TRUE))
+  # Return character vector of WKT values
+  out_wkt
+}
+
+
+#' Converts a bounding box to WKT and writes it to a column in the data.table by
+#' reference
+#'
+#' @param dt data.table containing bounding box column
+#' @param in_column name of bounding box column
+#' @param out_column name of WKT output column
+#'
+#' @returns copy of the data.table
+convert_bbox_column <- function(dt, in_column, out_column){
+  in_column = as.name(in_column)
+  column_class = class(dt[, eval(in_column)])
+  # Calculate non-missing bounding box WKT values
+  if(column_class == "list"){
+    dt[sapply(get(in_column), length) > 0, (out_column) := bbox_to_polygon(eval(in_column))]
+  } else if(column_class == "character"){
+    dt[!is.na(get(in_column)), (out_column) := bbox_to_polygon(eval(in_column))]
+  } else {
+    stop(paste("Unsupported column type", column_class))
+  }
+}
+
+
+#' Updates a data.table by reference, converting circle queries to points (as
+#' WKT) and radius columns.
+#'
+#' Unlike some other functions in this script, the data.table should NOT be
+#' subset before sending to this function.
+#'
+#' @param dt a data.table to update by reference
+#' @param circle_column string, name of the column containing CMR circle queries
+#' @param out_wkt_column string, name of the column to hold output WKT points
+#' @param out_radius_column string, name of the column to hold output circle
+#'   radii
+calculate_circle_columns <- function(dt, circle_column, out_wkt_column, out_radius_column){
+  circle_column_class = class(dt[, get(circle_column)]) 
+  
+  if(circle_column_class == "list"){
+    i_condition = dt[ , sapply(get(circle_column), length) > 0]
+  } else {
+    i_condition = dt[ , !is.na(get(circle_column))]
+  }
+  circle_values = lapply(dt[i_condition, get(circle_column)], parsed_circle_values)
+  
+  wkt_vec = unlist(circle_values, use.names = FALSE)[names(unlist(
+    circle_values)) == 'point']
+  radius_vec = as.integer(unlist(circle_values, use.names = FALSE)[names(unlist(
+    circle_values)) == 'radius'])
+
+  dt[i_condition, (out_wkt_column) := wkt_vec]
+  dt[i_condition, (out_radius_column) := radius_vec]
+}
+
+
+#' Convert CMR circle query CSV into a WKT point value and a radius.
+#'
+#' If multiple circles are provided, the WKT string returned is a MULTIPOINT
+#' containing the center of every circle. However, only a single radius will be
+#' returned, representing the mean of all circle radii from the input list.
+#'
+#' @param csv string or list of strings containing CMR circle queries (e.g.:
+#'   "-83,40,2000")
+#'
+#' @returns list with "point" (WKT string) and "radius" (numeric) items.
+parsed_circle_values <- function(csv){
+  parsed_circles = lapply(csv, function(x){
+    all_split = str_split_1(x, ',')
+    len_all_split = length(all_split)
+    if(len_all_split != 3){
+      notice = paste0("ERROR: got ", len_all_split, 
+                      " split values instead of expected 3")
+      warning(notice)
+      return(notice)
+    }
+    point = paste0("(", all_split[1], " ", all_split[2], ")")
+    radius = all_split[3]
+    list(point = point, radius = radius)
+  })
+  if(length(parsed_circles) > 1){
+    warning("Multiple circles provided; returning mean radius and aggregated MULTIPOINT feature\n")
+    list(point = paste0("MULTIPOINT (", paste(
+           sapply(parsed_circles, function(x){x[["point"]]}), collapse = ", "), ")"),
+         radius = mean(sapply(parsed_circles, function(x){as.numeric(x[["radius"]])})))
+  } else {
+    list(point = paste("POINT", parsed_circles[[1]][["point"]]), 
+         radius = parsed_circles[[1]][["radius"]])
+  }
+}
+
+
+#' Handy function for previewing contents of multiple data.table columns
+#'
+#' @param dt a data.table
+#' @param column_names a character vector of column names to preview
+DEBUG_columninfo <- function(dt, column_names){
+  for (column in column_names){
+    colclass = class(dt[,get(column)])
+    print(paste(column, "-", colclass))
+    if(colclass == "list"){
+      colValues = dt[!is.na(get(column)), get(column)]
+      colValues[sapply(colValues, length) == 0] <- NA_character_
+      print(paste("  e.g.:", first(na.omit(unlist(colValues)))))
+    } else {
+      print(paste("  e.g.:", dt[!is.na(get(column)), first(get(column))]))
+    }
+  }
+  invisible()
+}
